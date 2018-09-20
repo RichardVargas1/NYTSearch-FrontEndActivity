059d4231a8d2b3c1188d5d18f3208926e6b6f811
Ian Richard <ianrichard03@gmail.com>
2018-09-19 21:25:08 -0500

    I done messed up good

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,25 @@
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-queryUrl += '?' + $.param({
- 'api-key': "8335fc7b2a3545bcb71046bf50bee79e"
-});
-
-
-
-//getData();
 
 
     
+        
+    function getData(){
+        var mySearch = $("#searchText").val();
+        var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        queryUrl += '?' + $.param({
+            'api-key': "8335fc7b2a3545bcb71046bf50bee79e",
+            'q' : mySearch
 
-$.ajax({
-    url: queryURl,
-    method: "GET"
-})
-
-   .then(function(response) {
-        var myCopyright = (response.copyright);
-        console.log(myCopyright);
-    });
+        });
 
 
+   $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
+     
+        .then(function(response) {
+            var myResponse = (response.docs.snippet);
+            console.log(myResponse);
+        });
+     
+}
