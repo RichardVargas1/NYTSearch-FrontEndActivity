de6a7d2072b385e8b1f567e4d1c92e3580796f1f
Ian Richard <ianrichard03@gmail.com>
2018-09-19 20:52:14 -0500

    added ajax url and api key

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+queryUrl += '?' + $.param({
+ 'api-key': "8335fc7b2a3545bcb71046bf50bee79e"
+});
+
+
+
+getData();
+
+
+    
+
+$.ajax({
+    url: queryURl,
+    method: "GET"
+})
+
+   .then(function(response) {
+        var myCopyright = (response.copyright);
+        console.log(myCopyright);
+    });
+
+
