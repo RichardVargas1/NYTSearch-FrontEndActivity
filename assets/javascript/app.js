749a2a726f0ce6725c1942414738aedb68a52159
Ian Richard <ianrichard03@gmail.com>
2018-09-20 19:45:17 -0500

    fixed url, so response will work, just have to tie search and button somehow

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,31 @@
-$(document).ready(function(){
+//$(document).ready(function(){
 
-var mySearch = $("#searchText").val();
+var mySearch;
 var url  = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({'api-key': "267cf592692f45e68a43dbbf35e843a4",'q' : mySearch});
 var myResponse;
+
+
+$( ".search" ).click(function() {
+    getData();
+});
 
    $.ajax({
         url: url,
         method: 'GET'
     }).then(function(response) {
         
-        myResponse = response;
-        console.log(myResponse);
+        
+
+        
 
     });
-     
-    $(".search").click(function(){
-        getData();
-    });
-
 
     function getData(){
-        mySearch 
-        queryUrl += '?' + $.param({'api-key': "267cf592692f45e68a43dbbf35e843a4",'q' : mySearch})
+        alert("THis happend!");
+        mySearch = $("#searchText").val(); 
+        myResponse = response;
+        console.log(myResponse);   
     }   
 
-});
+//});
