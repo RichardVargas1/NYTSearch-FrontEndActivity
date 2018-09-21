6027699f396f11dbbdb885b75b8de48eb1fab03f
Ian Richard <ianrichard03@gmail.com>
2018-09-20 23:01:17 -0500

    fixed scope issue, all in doc ready, all children in click callback.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,24 @@
-//$(document).ready(function(){
-
-var mySearch;
-var url  = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({'api-key': "267cf592692f45e68a43dbbf35e843a4",'q' : mySearch});
-var myResponse;
-
-
-$( ".search" ).click(function() {
-    getData();
-});
-
-   $.ajax({
-        url: url,
-        method: 'GET'
-    }).then(function(response) {
+$(document).ready(function(){
+    $( ".search" ).click(function() {
+        getData();
+        console.log("SHOW ME");
+        var mySearch;
+        var url  = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += '?' + $.param({'api-key': "267cf592692f45e68a43dbbf35e843a4",'q' : mySearch});
+        var myResponse;
         
-        
-
-        
-
+        $.ajax({
+            url: url,
+            method: 'GET'
+        }).then(function(response) {
+            myResponse = response;    
+        });
+    
+        function getData(){
+            alert("THis happend!");
+            mySearch = $("#searchText").val(); 
+            
+            console.log(myResponse);   
+        }   
     });
-
-    function getData(){
-        alert("THis happend!");
-        mySearch = $("#searchText").val(); 
-        myResponse = response;
-        console.log(myResponse);   
-    }   
-
-//});
+});
