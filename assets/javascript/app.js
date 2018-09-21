28f0dc79a466d2cbe4966b951cea11aa39b820b3
Ian Richard <ianrichard03@gmail.com>
2018-09-20 23:21:20 -0500

    i have no idea what i'm doing.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,27 @@
 $(document).ready(function(){
+    
+    var mySearch;
+    var url  = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({'api-key': "267cf592692f45e68a43dbbf35e843a4",'q' : mySearch});
+    var myResponse;
+    
     $( ".search" ).click(function() {
         getData();
-        console.log("SHOW ME");
-        var mySearch;
-        var url  = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-        url += '?' + $.param({'api-key': "267cf592692f45e68a43dbbf35e843a4",'q' : mySearch});
-        var myResponse;
+        
         
         $.ajax({
             url: url,
             method: 'GET'
         }).then(function(response) {
-            myResponse = response;    
+            console.log(JSON.stringify(response));
+            console.log(mySearch);
         });
     
         function getData(){
             alert("THis happend!");
             mySearch = $("#searchText").val(); 
             
-            console.log(myResponse);   
+            
         }   
     });
 });
