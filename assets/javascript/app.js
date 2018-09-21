6e3f99875be4418e6aa8ddd8494a159db9da374a
mig9tx <migue.trujillo@utexas.edu>
2018-09-21 14:10:25 -0500

    removed action from form element in html.  added new js with the on click event under a document ready. set up the function with the call back function so that once a search term is used is pulls up the title of the article. miguel

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,47 @@
-$(document).ready(function(){
-    
-    var mySearch;
-    var url  = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({'api-key': "267cf592692f45e68a43dbbf35e843a4",'q' : mySearch});
-    var myResponse;
-    
-    $( ".search" ).click(function() {
-        getData();
-        
-        
-        $.ajax({
-            url: url,
-            method: 'GET'
-        }).then(function(response) {
-            console.log(JSON.stringify(response));
-            console.log(mySearch);
-        });
-    
-        function getData(){
-            alert("THis happend!");
-            mySearch = $("#searchText").val(); 
-            
-            
-        }   
+function searchArticle(inputTerm) {
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + inputTerm + "&api-key=8335fc7b2a3545bcb71046bf50bee79e";
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+    }).then(function (result) {
+        console.log(result);
+        $("#results").append("<br>Title: " + result.response.docs[0].headline.main + "<br>");
+    })
+}
+
+$(document).ready(function () {
+    $("#submit-button").on("click", function (event) {
+        var value = $("#sterm").val().trim();
+        searchArticle(value);
     });
 });
+
+
+// $(document).ready(function(){
+    
+//     var mySearch;
+//     var url  = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+//     url += '?' + $.param({'api-key': "267cf592692f45e68a43dbbf35e843a4",'q' : mySearch});
+//     var myResponse;
+    
+//     $( ".search" ).click(function() {
+//         getData();
+        
+        
+//         $.ajax({
+//             url: url,
+//             method: 'GET'
+//         }).then(function(response) {
+//             console.log(JSON.stringify(response));
+//             console.log(mySearch);
+//         });
+    
+//         function getData(){
+//             alert("THis happend!");
+//             mySearch = $("#searchText").val(); 
+            
+            
+//         }   
+//     });
+// });
